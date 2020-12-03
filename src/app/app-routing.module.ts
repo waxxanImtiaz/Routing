b227ddb24dd5f09fd483540d69828373806422d2
Imtiaz ali <imtiaz.ali@m3tech.com.pk>
2020-12-03 19:12:34 +0500

    Job details component implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DepartmentListComponent} from './department-list/department-list.component';
+import {DetailsComponent} from './details/details.component';
+import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/department', pathMatch: 'full'},
+  { path: 'department', component: DepartmentListComponent},
+  { path: 'employee', component: EmployeeListComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponent = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent, DetailsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, routingComponent} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import {Route} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DetailsComponent } from './details/details.component';
 
+
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent,
+    PageNotFoundComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +26,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
